2897e7be6a663483886271d28db2054281d80b62
Maureen Perez <maureen8694@gmail.com>
2022-01-07 16:43:34 -0500

    Inital commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string myName; // declaration syntax
+myName = "Maureen"; // initialized
+
+int myAge = 27; // declaring & initializing
+
+char firstInitial = 'M';
+
+bool isHungry = true;
+
+double myWeight = 175.8;
+
+decimal myCarSpeed = 60.5m;
+
+Console.WriteLine($"My name is {myName}. I am {myAge} years old. My initial is {firstInitial}.");
+Console.WriteLine($"I am hungry {isHungry}. I weigh {myWeight}. When I drive I go {myCarSpeed}.");
